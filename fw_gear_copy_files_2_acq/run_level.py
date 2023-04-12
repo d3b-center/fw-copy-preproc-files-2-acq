d17b9a1a55491cb8b06e132a1dff6b2a693d7d1a
Ariana <arianafamiliar@hotmail.com>
2023-04-12 17:56:25 -0400

    update run_level.py to correctly source container info

--- fw_gear_copy_files_2_acq/run_level.py
+++ fw_gear_copy_files_2_acq/run_level.py
@@ -6,6 +6,9 @@
 import logging
 
 # from flywheel import ApiException
+import flywheel
+fw_context = flywheel.GearContext()
+fw = fw_context.client
 
 log = logging.getLogger(__name__)
 
@@ -30,12 +33,20 @@ def get_analysis_run_level_and_hierarchy(gtk_context_client, destination_id):
 
     run_level = 'session'
     destination = gtk_context_client.get(destination_id)
+    analysis_id = fw_context.destination['id']
+
+    # print(destination)
     if destination.container_type == run_level:
 
         hierarchy["group"] = destination.parents["group"]
 
         for level in ["project", "subject", "session"]:
-            if destination.parents[level]:
+            if level == run_level:
+                analysis_container = fw.get(analysis_id)
+                # print(analysis_container)
+                # container = gtk_context_client.get(analysis_container.parent['id'])
+                hierarchy[f"{level}_label"] = analysis_container.label # if gear run at session level, this is the session label
+            elif destination.parents[level]:
                 container = gtk_context_client.get(destination.parents[level])
                 hierarchy[f"{level}_label"] = container.label
     else:
